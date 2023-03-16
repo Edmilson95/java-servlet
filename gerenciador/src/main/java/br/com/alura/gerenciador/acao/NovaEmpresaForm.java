34d20c09747e12cf0b3087f0597f1d6c69b9434f
Edmilson Ferreira <100481668+Edmilson95@users.noreply.github.com>
2023-03-16 09:23:47 -0300

    refatorando o código

--- gerenciador/src/main/java/br/com/alura/gerenciador/acao/NovaEmpresaForm.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/acao/NovaEmpresaForm.java
@@ -6,44 +6,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class NovaEmpresaForm {
+public class NovaEmpresaForm implements Acao{
 
 	public String executa(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		return "forward:formNovaEmpresa.jsp";
 
-	}
-
-}
-
-class Veiculo {
-
-	public void acelerar() {
-
-		System.out.println("Veiculo acelerando");
-
-	}
-
-}
-
-class Carro extends Veiculo {
-
-	public void acelerar() {
-
-		System.out.println("Carro acelerando");
-
-	}
-
-}
-
-class Teste {
-
-	public static void main(String[] args) {
-
-		Veiculo veiculo = new Carro();
-
-		veiculo.acelerar();
 	}
 
 }
--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.gerenciador.acao.Acao;
 import br.com.alura.gerenciador.acao.AlteraEmpresa;
 import br.com.alura.gerenciador.acao.ListaEmpresas;
 import br.com.alura.gerenciador.acao.MostraEmpresa;
@@ -28,30 +29,16 @@ public class UnicaEntradaServlet extends HttpServlet {
 
 		String paramAcao = request.getParameter("acao");
 		
-		String nome = null;
+		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;
 		
-		if (paramAcao.equals("ListaEmpresas")) {
-			ListaEmpresas acao = new ListaEmpresas();
+		String nome;
+		try {
+			Class classe = Class.forName(nomeDaClasse);//carrega a classe com o nome
+			Acao acao = (Acao) classe.newInstance();
 			nome = acao.executa(request, response);
-			
-		} else if (paramAcao.equals("RemoveEmpresa")) {
-			RemoveEmpresa acao = new RemoveEmpresa();
-			nome = acao.executa(request, response);
-		} else if (paramAcao.equals("MostraEmpresa")) {
-			MostraEmpresa acao = new MostraEmpresa();
-			nome = acao.executa(request, response);
-		} else if (paramAcao.equals("AlteraEmpresa")) {
-			AlteraEmpresa acao = new AlteraEmpresa();
-			nome = acao.executa(request, response);
-		} else if (paramAcao.equals("NovaEmpresa")) {
-			NovaEmpresa acao = new NovaEmpresa();
-			nome = acao.executa(request, response);
-		} else if (paramAcao.equals("NovaEmpresaForm")) {
-			NovaEmpresaForm acao = new NovaEmpresaForm();
-			nome = acao.executa(request, response);
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+			throw new ServletException(e);
 		}
-		
-		
 		
 		String[] tipoEEndereco = nome.split(":");
 		if(tipoEEndereco[0].equals("forward")) {
@@ -60,6 +47,30 @@ public class UnicaEntradaServlet extends HttpServlet {
 		} else {
 			response.sendRedirect(tipoEEndereco[1]);
 		}
+		
+//		String nome = null;
+//		
+//		if (paramAcao.equals("ListaEmpresas")) {
+//			ListaEmpresas acao = new ListaEmpresas();
+//			nome = acao.executa(request, response);
+//			
+//		} else if (paramAcao.equals("RemoveEmpresa")) {
+//			RemoveEmpresa acao = new RemoveEmpresa();
+//			nome = acao.executa(request, response);
+//		} else if (paramAcao.equals("MostraEmpresa")) {
+//			MostraEmpresa acao = new MostraEmpresa();
+//			nome = acao.executa(request, response);
+//		} else if (paramAcao.equals("AlteraEmpresa")) {
+//			AlteraEmpresa acao = new AlteraEmpresa();
+//			nome = acao.executa(request, response);
+//		} else if (paramAcao.equals("NovaEmpresa")) {
+//			NovaEmpresa acao = new NovaEmpresa();
+//			nome = acao.executa(request, response);
+//		} else if (paramAcao.equals("NovaEmpresaForm")) {
+//			NovaEmpresaForm acao = new NovaEmpresaForm();
+//			nome = acao.executa(request, response);
+//		}
+		
 	
 	}
 
