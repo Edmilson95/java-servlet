1f008644ac81ed5eb3ae75e78e6d7cf9316ca9aa
Edmilson Ferreira <100481668+Edmilson95@users.noreply.github.com>
2023-03-16 18:31:12 -0300

    implementando logout

--- gerenciador/WebContent/WEB-INF/view/formNovaEmpresa.jsp
+++ gerenciador/WebContent/WEB-INF/view/formNovaEmpresa.jsp
@@ -1,5 +1,5 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<c:url value="/entrada" var="linkEntradaNovaEmpresa"/>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<c:url value="/entrada" var="linkEntradaNovaEmpresa" />
 
 <!DOCTYPE html>
 <html>
@@ -9,14 +9,14 @@
 </head>
 <body>
 
+	<c:import url="logout-parcial.jsp" />
+
+
 	<form action="${linkEntradaNovaEmpresa }" method="post">
-	
-		Nome: <input type="text" name="nome"  />
-		Data Abertura: <input type="text" name="data"  />
-		
-		<input type="hidden" name="acao" value="NovaEmpresa" />
-	
-		<input type="submit" />
+
+		Nome: <input type="text" name="nome" /> Data Abertura: <input
+			type="text" name="data" /> <input type="hidden" name="acao"
+			value="NovaEmpresa" /> <input type="submit" />
 	</form>
 
 </body>
--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.acao.Acao;
 import br.com.alura.gerenciador.acao.AlteraEmpresa;
@@ -29,25 +30,35 @@ public class UnicaEntradaServlet extends HttpServlet {
 
 		String paramAcao = request.getParameter("acao");
 		
-		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;
+		HttpSession sessao = request.getSession();
+		boolean usuarioNaoEstaLogado = (sessao.getAttribute("usuarioLogado") == null);
+		boolean ehUmaAcaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("LoginForm"));
 		
+		if (ehUmaAcaoProtegida && usuarioNaoEstaLogado) {
+			response.sendRedirect("entrada?acao=LoginForm");
+			return;
+		}
+
+
+		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;
+
 		String nome;
 		try {
-			Class classe = Class.forName(nomeDaClasse);//carrega a classe com o nome
+			Class classe = Class.forName(nomeDaClasse);// carrega a classe com o nome
 			Acao acao = (Acao) classe.newInstance();
 			nome = acao.executa(request, response);
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
 			throw new ServletException(e);
 		}
-		
+
 		String[] tipoEEndereco = nome.split(":");
-		if(tipoEEndereco[0].equals("forward")) {
+		if (tipoEEndereco[0].equals("forward")) {
 			RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/view/" + tipoEEndereco[1]);
 			rd.forward(request, response);
 		} else {
 			response.sendRedirect(tipoEEndereco[1]);
 		}
-		
+
 //		String nome = null;
 //		
 //		if (paramAcao.equals("ListaEmpresas")) {
@@ -70,8 +81,7 @@ public class UnicaEntradaServlet extends HttpServlet {
 //			NovaEmpresaForm acao = new NovaEmpresaForm();
 //			nome = acao.executa(request, response);
 //		}
-		
-	
+
 	}
 
 }
